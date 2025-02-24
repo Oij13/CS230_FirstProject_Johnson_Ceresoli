f572bf2092d05a33612019c2245808275ccdc8d2
Oij13 <oij13@icloud.com>
2025-02-24 10:50:48 -0500

    Init Commit

---
+++ src/main/java/pepasm.java
@@ -0,0 +1,8 @@
+public class pepasm {
+    public static void main(String[] args) {
+        if (args.length > 0){
+            System.out.println("args[0]: " + args[0]);
+            System.out.println("args[1]: " +args[1]);
+        }
+    }
+}
