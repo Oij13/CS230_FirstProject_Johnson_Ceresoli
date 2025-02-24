3a89d198cd6dd6d1505392d96b935463cbe72a58
Oij13 <oij13@icloud.com>
2025-02-24 11:41:41 -0500

    Innit commit

--- src/main/java/pepasm.java
+++ src/main/java/pepasm.java
@@ -2,7 +2,7 @@ public class pepasm {
     public static void main(String[] args) {
         if (args.length > 0){
             System.out.println("args[0]: " + args[0]);
-            System.out.println("args[1]: " +args[1]);
+            System.out.println("args[1]: " + args[1]);
         }
     }
 }
