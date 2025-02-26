ffbe7ce007a62c39f3f9f37072f9c2730a54ad28
Oij13 <oij13@icloud.com>
2025-02-25 20:12:23 -0500

    Add pep files for parameters and production code created

--- src/main/java/pepasm.java
+++ src/main/java/pepasm.java
@@ -1,8 +1,57 @@
+
+import java.io.*;
+import java.util.HashMap;
+
 public class pepasm {
     public static void main(String[] args) {
-        if (args.length > 0){
-            System.out.println("args[0]: " + args[0]);
-            System.out.println("args[1]: " + args[1]);
+        HashMap<String, String> instructionMap = new HashMap<>();
+        HashMap<String, String> addressingModeMap = new HashMap<>();
+
+
+        instructionMap.put("LDBA", "D0");
+        instructionMap.put("STBA", "F1");
+        instructionMap.put("STOP", "00");
+        instructionMap.put("STWA", "F5");
+        instructionMap.put("LDWA", "D5");
+        instructionMap.put("ANDA", "C5");
+        instructionMap.put("ASLA", "04");
+        instructionMap.put("ASRA", "05");
+        instructionMap.put("CPBA", "C0");
+        instructionMap.put("BRNE", "28");
+
+        addressingModeMap.put("i", "00");
+        addressingModeMap.put("d", "16");
+
+
+        String fileName = args[0];
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+
+            StringBuilder machineCode = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                System.out.println("Reading line: " + line);
+                String[] parts = line.split("\\s+");
+
+                if (instructionMap.containsKey(parts[0])) {
+                    String instruction = instructionMap.get(parts[0]);
+                    if (parts[0].equals("STOP")) {
+                        machineCode.append(instruction).append(" ");
+                        break;
+                    }
+                    if (parts.length >= 2) {
+                        String operand = parts[1].replace("0x", "").replace(",", "");
+
+                        String address = addressingModeMap.get(parts[2]);
+                        machineCode.append(instruction).append(" ").append(operand.substring(0,2)).append(" ").append(address).append(" ");
+                    }
+                    else{
+                        machineCode.append(instruction).append(" ");
+                    }
+                }
+            }
+            System.out.println(machineCode.toString().trim());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
