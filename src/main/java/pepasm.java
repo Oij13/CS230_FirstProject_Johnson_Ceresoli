92486523850978cde81d50d0af9d7223946aaa9a
Oij13 <oij13@icloud.com>
2025-02-26 10:35:29 -0500

    Fixes spacing within machine code

--- src/main/java/pepasm.java
+++ src/main/java/pepasm.java
@@ -1,57 +1,58 @@
-
 import java.io.*;
 import java.util.HashMap;
 
-public class pepasm {
-    public static void main(String[] args) {
-        HashMap<String, String> instructionMap = new HashMap<>();
-        HashMap<String, String> addressingModeMap = new HashMap<>();
+    public class pepasm {
+        public static void main(String[] args) {
+            HashMap<String, String> instructionMap = new HashMap<>();
+            HashMap<String, String> addressingModeMap = new HashMap<>();
 
 
-        instructionMap.put("LDBA", "D0");
-        instructionMap.put("STBA", "F1");
-        instructionMap.put("STOP", "00");
-        instructionMap.put("STWA", "F5");
-        instructionMap.put("LDWA", "D5");
-        instructionMap.put("ANDA", "C5");
-        instructionMap.put("ASLA", "04");
-        instructionMap.put("ASRA", "05");
-        instructionMap.put("CPBA", "C0");
-        instructionMap.put("BRNE", "28");
+            instructionMap.put("LDBA", "D0");
+            instructionMap.put("STBA", "F1");
+            instructionMap.put("STOP", "00");
+            instructionMap.put("STWA", "F5");
+            instructionMap.put("LDWA", "D5");
+            instructionMap.put("ANDA", "C5");
+            instructionMap.put("ASLA", "04");
+            instructionMap.put("ASRA", "05");
+            instructionMap.put("CPBA", "C0");
+            instructionMap.put("BRNE", "28");
 
-        addressingModeMap.put("i", "00");
-        addressingModeMap.put("d", "16");
+            addressingModeMap.put("i", "00");
+            addressingModeMap.put("d", "16");
+            String fileName = args[0];
+            try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+                String line;
+                StringBuilder machineCode = new StringBuilder();
+                while ((line = br.readLine()) != null) {
+                    System.out.println("Reading line: " + line);
+                    String[] parts = line.split("\\s+");
 
+                    if (instructionMap.containsKey(parts[0])) {
+                        String instruction = instructionMap.get(parts[0]);
+                        if (parts[0].equals("STOP")) {
+                            machineCode.append(instruction);
+                            break;
+                        }
+                        if (parts.length >= 2) {
+                            String operand = parts[1].replace("0x", "").replace(",", "");
 
-        String fileName = args[0];
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String line;
+                            String address = addressingModeMap.get(parts[2]);
+                            machineCode.append(instruction).append(operand).append(address);
 
-            StringBuilder machineCode = new StringBuilder();
-            while ((line = br.readLine()) != null) {
-                System.out.println("Reading line: " + line);
-                String[] parts = line.split("\\s+");
-
-                if (instructionMap.containsKey(parts[0])) {
-                    String instruction = instructionMap.get(parts[0]);
-                    if (parts[0].equals("STOP")) {
-                        machineCode.append(instruction).append(" ");
-                        break;
-                    }
-                    if (parts.length >= 2) {
-                        String operand = parts[1].replace("0x", "").replace(",", "");
-
-                        String address = addressingModeMap.get(parts[2]);
-                        machineCode.append(instruction).append(" ").append(operand.substring(0,2)).append(" ").append(address).append(" ");
-                    }
-                    else{
-                        machineCode.append(instruction).append(" ");
+                        }
+                        else{
+                            machineCode.append(instruction);
+                        }
                     }
                 }
+                for (int i = 2; i < machineCode.length(); i += 3) {
+                    machineCode.insert(i, ' ');
+                }
+                System.out.println(machineCode);
+            } catch (IOException e) {
+                System.out.println("File Error: " + e.getMessage());
             }
-            System.out.println(machineCode.toString().trim());
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-}
+
